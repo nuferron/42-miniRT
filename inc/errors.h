9e9848d6a04d11a35484d9f75e8005933fca15c1
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-10 22:14:30 +0100

    added macros

--- inc/errors.h
+++ inc/errors.h
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:07:57 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/10 17:35:20 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/10 21:21:56 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 
 #define ERROR	RED "Error\n" RESET
 #define ARGS	ERROR "Invalid number of arguments: " \
-				"you must provide ONE file *.rt\n"
-#define FNF		ERROR "File not found: %s\n"
-#define TYPE	ERROR "The file must end with the extension \".rt\"\n"
-#define EMPTY	ERROR "This file is empty!\n"
-#define LINE	ERROR "Invalid line: %s\nProgram stopped\n"
+				"you must provide ONE file *.rt\n" RESET
+#define FNF		ERROR "File not found: %s\n" RESET
+#define TYPE	ERROR "The file must end with the extension \".rt\"\n" RESET
+#define EMPTY	ERROR "This file is empty!\n" RESET
+#define LINE	ERROR "Invalid line: \"%s\"\nProgram stopped\n" RESET
+#define MEM		ERROR "Memory problems\n" RESET
+#define RANGE	ERROR "The input (%s)is either invalid" \
+				"or out of range (%s)\n" RESET
 
 #endif
