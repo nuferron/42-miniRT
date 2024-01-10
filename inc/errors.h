29f585bcb78a72b82b284e0dd27a8e8d8e1dc5f9
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-10 18:01:29 +0100

    upgraded error messages

--- inc/errors.h
+++ inc/errors.h
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:07:57 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/10 15:48:02 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/10 17:35:20 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 #define ERROR	RED "Error\n" RESET
 #define ARGS	ERROR "Invalid number of arguments: " \
-				"you must provide ONE file *.rt"
-#define FNF		ERROR "File not found:"
-#define TYPE	ERROR "The file must end with the extension \".rt\""
+				"you must provide ONE file *.rt\n"
+#define FNF		ERROR "File not found: %s\n"
+#define TYPE	ERROR "The file must end with the extension \".rt\"\n"
+#define EMPTY	ERROR "This file is empty!\n"
+#define LINE	ERROR "Invalid line: %s\nProgram stopped\n"
 
 #endif
