4c7dcddb624fa3cd0df804b1121770d52d9e7df6
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-10 15:27:45 +0100

    starting to work on it

--- inc/messages.h
+++ inc/messages.h
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   miniRT.h                                           :+:      :+:    :+:   */
+/*   messages.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 19:59:25 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/10 14:23:53 by nuferron         ###   ########.fr       */
+/*   Created: 2024/01/10 13:07:57 by nuferron          #+#    #+#             */
+/*   Updated: 2024/01/10 14:48:32 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIRT_H
-# define MINIRT_H
+#ifndef MESSAGES_H
+# define MESSAGES_H
 
-#include <math.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "messages.h"
-#include "libft/inc/libft.h"
-
-int	check_input(int argc, char **argv);
+#define ARGS			"Error\nInvalid number of arguments: you must provide ONE file *.rt"
+#define FILE_NOT_FOUND	"Error\nFile not found : %s"
 
 #endif
