b37ef96b3f779dfa50133bb6d6811e5618b8d371
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-16 17:32:03 +0100

    added more error messages

--- inc/errors.h
+++ inc/errors.h
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:07:57 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/11 22:12:19 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:39:09 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,8 @@
 #define RANGE	ERROR "In line \"%s\", the input is either invalid " \
 				"or out of range ([%d - %d])\n" RESET
 #define REPE	ERROR "Element repeated: %s\n" RESET
+#define INT		ERROR "Found float when expecting integer: %s\n" RESET
+#define PARAM	ERROR "Invalid number of parameters in line \"%s\"\n" RESET
+#define ELEM	ERROR "Element not found: %s\n" RESET
 
 #endif
