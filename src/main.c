fd8bcc00e8458f1a389656e0c354632c9c03e419
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-16 17:32:28 +0100

    added initializing ft

--- src/main.c
+++ src/main.c
@@ -6,18 +6,36 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:58:50 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/11 20:44:35 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/16 17:09:49 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+void	init_sc(t_sc *sc)
+{
+	sc->amb.ratio = -1;
+	sc->cam.fov = -1;
+	sc->light.b = -1;
+	sc->sp.obj = NULL;
+	sc->pl.obj = NULL;
+	sc->cy.obj = NULL;
+	sc->sp.size = sizeof(t_sp);
+	sc->pl.size = sizeof(t_pl);
+	sc->cy.size = sizeof(t_cy);
+	sc->sp.total = 0;
+	sc->pl.total = 0;
+	sc->cy.total = 0;
+}
+
 int	main(int argc, char **argv)
 {
-	static t_sc	sc = {NULL, NULL, NULL, NULL, NULL, NULL};
+	static t_sc	sc;
 
+	init_sc(&sc);
 	if (check_input(argc, argv, &sc))
 		return (1);
-	//printf("main: sc sp x %f\n", sc.sp->x);
+	printf("main: L pos x %f\n", sc.light.pos.x);
+	free_all(&sc);
 	return (0);
 }
