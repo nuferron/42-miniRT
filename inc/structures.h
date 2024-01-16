abfce03e7a05388f1260d7b347384ea0d8970169
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-16 19:37:23 +0100

    added comments + norminetted

--- inc/structures.h
+++ inc/structures.h
@@ -6,71 +6,74 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:17:32 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/16 14:31:38 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/16 19:36:03 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
 
-typedef struct	s_item
+/*Structure that can contain a t_sp, a t_pl or a t_cy pointer in void *obj
+Size is a sizeof the structure and total the number we have of them*/
+typedef struct s_item
 {
 	void	*obj;
 	size_t	size;
 	int		total;
-} t_item;
+}	t_item;
 
+/*Structure for coordinates ("absolute" or normalized)*/
 typedef struct s_vec
 {
 	float	x;
 	float	y;
 	float	z;
-} t_vec;
+}	t_vec;
 
-typedef struct s_amb	//AMBIENT LIGHTNING
+typedef struct s_amb //AMBIENT LIGHTNING
 {
 	float	ratio;	//ambient lightning ratio [0.0- 1.0]
 	int		rgb[3];
-} t_amb;
+}	t_amb;
 
-typedef struct s_cam	//CAMERA
+typedef struct s_cam //CAMERA
 {
 	t_vec	pos;
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	float	fov;	//horizontal field of view in degrees [0 - 180]
-} t_cam;
+}	t_cam;
 
 typedef struct s_light
 {
 	t_vec	pos;
 	float	b;		//light brightness ratio [0.0 - 1.0]
 	int		rgb[3];	//(bonus)
-} t_light;
+}	t_light;
 
-typedef struct s_sp		//SPHERE
+typedef struct s_sp //SPHERE
 {
 	t_vec	pos;
 	float	d;		//diameter
 	int		rgb[3];
-} t_sp;
+}	t_sp;
 
-typedef struct s_pl		//PLANE
+typedef struct s_pl //PLANE
 {
 	t_vec	pos;
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	int		rgb[3];
-} t_pl;
+}	t_pl;
 
-typedef struct s_cy		//CYLINDER
+typedef struct s_cy //CYLINDER
 {
 	t_vec	pos;
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	float	d;		//diameter
 	float	h;		//height
 	int		rgb[3];
-} t_cy;
+}	t_cy;
 
-typedef struct s_sc		//SCENE
+typedef struct s_sc //SCENE
 {
 	t_amb		amb;
 	t_cam		cam;
@@ -78,6 +81,6 @@ typedef struct s_sc		//SCENE
 	t_item		sp;
 	t_item		pl;
 	t_item		cy;
-} t_sc;
+}	t_sc;
 
 #endif
