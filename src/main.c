bdb65af109d5b814658c45c96ccf1db69da76722
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-18 18:13:55 +0100

    testing coord translation

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:58:50 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/16 17:09:49 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/18 17:45:34 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,10 @@ int	main(int argc, char **argv)
 	if (check_input(argc, argv, &sc))
 		return (1);
 	printf("main: L pos x %f\n", sc.light.pos.x);
+	printf("main: C pos x %f\n", sc.cam.pos.x);
+	coord_transformation(&sc);
+	printf("main: L pos x %f\n", sc.light.pos.x);
+	printf("main: C pos x %f\n", sc.cam.pos.x);
 	free_all(&sc);
 	return (0);
 }
