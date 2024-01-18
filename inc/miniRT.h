08a4914a629f210d7a448b52a4873a0406162d03
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-01-18 18:17:04 +0100

    now ft only used in input are not include in the general header

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:59:25 by nuferron          #+#    #+#             */
-/*   Updated: 2024/01/16 15:27:22 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/01/18 18:06:16 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,6 @@
 #include "structures.h"
 #include "libft/inc/libft.h"
 #include "ft_dprintf/inc/libftprintf.h"
-
-/* CHECKING INPUT + INITIALIZING STRUCTS */
-int		check_input(int argc, char **argv, t_sc *sc);
-int		get_ambient(char *line, int i, t_amb *amb);
-int		get_camera(char *line, int i, t_cam *cam);
-int		get_light(char *line, int i, t_light *light);
-int		get_sphere(char *line, int i, t_item *sp);
-int		get_plane(char *line, int i, t_item *pl);
-int		get_cylinder(char *line, int i, t_item *cy);
-
-/* INITIALIZE STRUCTS VARIABLES */
-float	check_range(char *line, char type, int i);
-int		init_vec(t_vec *vec, char *line, int *i, char t);
-int		set_rgb(int *rgb, char *line, int i);
 
 /* MEMORY UTILS */
 void	free_all(t_sc *sc);
