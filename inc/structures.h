ab6a1c08e9102c70cd32b3c49e95e5b3c4d23d45
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-02-07 21:10:32 +0100

    using t_point when it's a point and t_vec when it's a vec

--- inc/structures.h
+++ inc/structures.h
@@ -6,7 +6,7 @@
 /*   By: nzhuzhle <nzhuzhle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:17:32 by nuferron          #+#    #+#             */
-/*   Updated: 2024/02/07 16:48:04 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/02/07 21:08:54 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ typedef struct s_vec
 	float	z;
 }	t_vec;
 
+typedef struct s_vec t_point;
+
 typedef struct s_amb //AMBIENT LIGHTNING
 {
 	float	ratio;	//ambient lightning ratio [0.0- 1.0]
@@ -40,35 +42,35 @@ typedef struct s_amb //AMBIENT LIGHTNING
 
 typedef struct s_cam //CAMERA
 {
-	t_vec	pos;	//center point
+	t_point	pos;	//center point
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	float	fov;	//horizontal field of view taken DEG [0 - 180] saved as RAD
 }	t_cam;
 
 typedef struct s_light
 {
-	t_vec	pos;	//center point
+	t_point	pos;	//center point
 	float	b;		//light brightness ratio [0.0 - 1.0]
 	int		rgb[3];	//(bonus)
 }	t_light;
 
 typedef struct s_sp //SPHERE
 {
-	t_vec	pos;	//center point
+	t_point	pos;	//center point
 	float	r;		//radius
 	int		rgb[3];
 }	t_sp;
 
 typedef struct s_pl //PLANE
 {
-	t_vec	pos;	//center point
+	t_point	pos;	//center point
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	int		rgb[3];
 }	t_pl;
 
 typedef struct s_cy //CYLINDER
 {
-	t_vec	pos;	//center point
+	t_point	pos;	//center point
 	t_vec	nov;	//3D normalized orientation vector for x [-1.0 - 1.0]
 	float	r;		//radius
 	float	h;		//height
@@ -96,7 +98,7 @@ typedef struct s_mlx
 
 typedef struct s_screen
 {
-	t_vec	center;
+	t_point	center;
 	float	width;
 	float	pix_rat;
 }	t_screen;
