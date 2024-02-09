d1c867c79d431f71cf580f50bfea7c8216e7cf91
Dina Zhuzhleva <106472944+DinaGala@users.noreply.github.com>
2024-02-09 18:58:20 +0100

    some comments

--- src/intersect/sphere_intersect.c
+++ src/intersect/sphere_intersect.c
@@ -6,7 +6,7 @@
 /*   By: nzhuzhle <nzhuzhle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 17:58:05 by nzhuzhle          #+#    #+#             */
-/*   Updated: 2024/02/09 00:51:28 by nzhuzhle         ###   ########.fr       */
+/*   Updated: 2024/02/09 18:57:26 by nzhuzhle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	sph_intersect(t_sp *sp, t_ray *ray)
 	t_point	p2;
 
 	var.oc = substr_vec(&ray->ray_orig, &sp->pos);
-	var.vo = substr_vec(&ray->ray_vec, &ray->ray_orig); // D
-	var.vo_dot = dot_prod(&var.vo, &var.vo); // k1
+	var.vo = substr_vec(&ray->ray_vec, &ray->ray_orig); // D probably add it to the ray struct?
+	var.vo_dot = dot_prod(&var.vo, &var.vo); // k1 - probably add it to the ray struct?
 	var.k2 = 2 * dot_prod(&var.oc, &var.vo);
 	var.discr = var.k2 * var.k2 - 4 * var.vo_dot * (dot_prod(&var.oc, &var.oc) - sp->r * sp->r);
 	if (var.discr < 0)
@@ -29,11 +29,12 @@ int	sph_intersect(t_sp *sp, t_ray *ray)
 	else if (var.discr == 0)
 		return (-var.k2 / (2 * var.vo_dot));	// here what we do if there is one point
 	v1 = mult_new(&var.vo, (-var.k2 - var.discr) / (2 * var.vo_dot));  // - discr
-	p1 = sum_vec(&ray->ray_orig, &v1); 
-	mult_fac(&var.vo, (-var.k2 + var.discr) / (2 * var.vo_dot));  // + discr
-	p2 = sum_vec(&ray->ray_orig, &var.vo); 
+	p1 = sum_vec(&ray->ray_orig, &v1); //first check the case if t < 0 (behind the camera)
 	if (dist(&p1, &ray->ray_orig) < ray->dist)
 		ray->dist = dist(&p1, &ray->ray_orig);
+		//save the point in the ray->hit
+	mult_fac(&var.vo, (-var.k2 + var.discr) / (2 * var.vo_dot));  // + discr
+	p2 = sum_vec(&ray->ray_orig, &var.vo); 
 	if (dist(&p2, &ray->ray_orig) < ray->dist)
 		ray->dist = dist(&p2, &ray->ray_orig);
 	// how to save the intersection point?
