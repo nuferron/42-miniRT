8677faad1d64b8dd81ab2fca46b85710cade3a8a
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-02-21 10:55:49 +0100

    working with colors

--- src/screen/color.c
+++ src/screen/color.c
@@ -6,24 +6,56 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 12:18:22 by nuferron          #+#    #+#             */
-/*   Updated: 2024/02/12 13:43:25 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/02/20 21:50:31 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int	rgb_to_hex(int r, int g, int b)
+/*unsigned int	rgb_to_hex(int r, int g, int b)
 {
 	return (r << 16 | g << 8 | b);
+}*/
+
+unsigned int	rgb_to_hex(int *rgb)
+{
+	return (rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
 }
 
-void	obj_color(t_sc *sc, int *color, t_vec *light, t_vec *norm)
+void	color_intensity(int *rgb, float intensity)
 {
-	double	dot;
+	rgb[0] *= intensity;
+	rgb[1] *= intensity;
+	rgb[2] *= intensity;
+}
 
-	dot = dot_prod(light, norm);
-	if (dot <= 0)
-		*color = 0x000000;
-	else
-		*color = *color + sc->light.b * dot + sc->amb.ratio;
+void	rerange_rgb(int *color)
+{
+	int	i;
+
+	i = 0;
+	while (i < 2)
+	{
+		if (color[i] > 255)
+			color[i] = 255;
+		else if (color[i] < 0)
+			color[i] = 0;
+		i++;
+	}
+}
+
+void	add_color(int *color, int *add)
+{
+	color[0] += add[0];
+	color[1] += add[1];
+	color[2] += add[2];
+	rerange_rgb(color);
+}
+
+void	multiply_color(int *color, int *mult)
+{
+	color[0] = color[0] * mult[0] / 255;
+	color[1] = color[1] * mult[1] / 255;
+	color[2] = color[2] * mult[2] / 255;
+	rerange_rgb(color);
 }
