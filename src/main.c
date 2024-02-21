a427adbed434ad1118f19a86defc1c70aafe8bf4
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-02-21 10:50:17 +0100

    deleted prints

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nzhuzhle <nzhuzhle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 19:58:50 by nuferron          #+#    #+#             */
-/*   Updated: 2024/02/14 23:09:48 by nzhuzhle         ###   ########.fr       */
+/*   Updated: 2024/02/20 18:07:21 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,7 @@ int	main(int argc, char **argv)
 	//printf("mlx %zu\ncam %zu\namb %zu\nli %zu\nit %zu\nscr %zu\n", sizeof(t_mlx), sizeof(t_cam), sizeof(t_amb), sizeof(t_light), sizeof(t_item), sizeof(t_screen));
 	if (check_input(argc, argv, &sc))
 		return (1);
-	printf("main: L pos x %f\n", sc.light.pos.x);
-	printf("main: C pos x %f\n", sc.cam.pos.x);
 	coord_transformation(&sc);
-	printf("main: L pos x %f\n", sc.light.pos.x);
-	printf("main: C pos x %f\n", sc.cam.pos.x);
   	init_mlx(&sc);
 	draw_scene(&sc);
 	maintain_mlx(&sc);
