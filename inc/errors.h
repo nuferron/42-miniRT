8746d92d5f2cfc68a7e2dcf434fa39d5cff7372c
Nuria Ferron Ferriz <nuriaf207@gmail.com>
2024-03-05 20:13:45 +0100

    added warning for cam inside obj

--- inc/errors.h
+++ inc/errors.h
@@ -6,7 +6,7 @@
 /*   By: nuferron <nuferron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 13:07:57 by nuferron          #+#    #+#             */
-/*   Updated: 2024/03/05 15:08:14 by nuferron         ###   ########.fr       */
+/*   Updated: 2024/03/05 19:10:17 by nuferron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@
 # define REC "We recommend you to work with lower numbers\n"
 # define UNNORM "Unnormalized vector found in line "
 # define NORM "We'll work with its normalized version\n"
+# define BLIND "The camera appears to be inside an object\n"
 
 #endif
